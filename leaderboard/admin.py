03b95eb4a6eee05169e6c809feb0b5ec6fb4fdbc
Pritam <itspratap255@gmail.com>
2026-03-30 15:36:03 +0530

    create admin pannel with account and leaderboard//some issues fix

--- leaderboard/admin.py
+++ leaderboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+#home page ke liye admin panel me user profile ko register karna zaroori hai taki hum uske data ko manage kar sakein.
+#home page models and dashboard page models are not created by me .... first i created them then i deleted them and then i created them again because of some errors but now they are working fine.
+from .models import UserProfile
+
+admin.site.register(UserProfile)
